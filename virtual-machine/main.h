f2cf6e10a54c99e07b00c2183a905df9ce051d08
broken-admin <brokengaming50@gmail.com>
2020-11-28 01:36:58 -0600

    Remove color definitions in virtual-machine/main.h - Unused color definitions in virtual-machine/main.h

--- virtual-machine/main.h
+++ virtual-machine/main.h
@@ -7,12 +7,6 @@
 
 #define CRST "\x1B[0m"  // Reset to default
 #define CBLK "\x1B[30m" // Black
-#define CRED "\x1B[31m" // Red
-#define CGRN "\x1B[32m" // Green
-#define CYEL "\x1B[33m" // Yellow
-#define CBLU "\x1B[34m" // Blue
-#define CMAG "\x1B[35m" // Magenta
-#define CCYN "\x1B[36m" // Cyan
 #define CWHT "\x1B[37m" // White
 
 #define SPT  "\x1B[0m\u250A" // Seperator of default color
